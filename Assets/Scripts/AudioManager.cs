9b084db4463cec96f40f395e221005ed5a219b85
Michaela97 <31960647+Michaela97@users.noreply.github.com>
2020-10-23 19:04:50 +0200

    enviroment assets added

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    void Awake()
+    {
+        foreach (var sound in sounds)
+        {
+            sound.source = gameObject.AddComponent<AudioSource>();
+
+            sound.source.clip = sound.clip;
+            sound.source.volume = sound.volume;
+            sound.source.loop = sound.loop;
+        }
+    }
+
+    private void Start()
+    {
+       Play("backgroundMusic");
+       Play("rain");
+
+       StartCoroutine(ThunderSound());
+    }
+
+    private IEnumerator ThunderSound()
+    {
+        while (true)
+        {
+            Play("thunder");
+            var value = Random.Range(7, 10);
+
+            yield return new WaitForSeconds(value);
+        }
+
+    }
+
+    public void Play(string soundName)
+    {
+        var sound = Array.Find(sounds, s => s.name == soundName);
+
+        try
+        {
+            sound.source.Play();
+        }
+        catch (NullReferenceException e)
+        {
+            Debug.Log("There is no audio with name:" + soundName);
+        }
+    }
+
+    public void Stop(string soundName)
+    {
+        var sound = Array.Find(sounds, s => s.name == soundName);
+
+        try
+        {
+            sound.source.Stop();
+        }
+        catch (NullReferenceException e)
+        {
+            Debug.Log("There is no audio with name:" + soundName);
+        }
+    }
+}
--- Assets/Scripts/Sound.cs
+++ Assets/Scripts/Sound.cs
@@ -1,7 +1,13 @@
-﻿namespace DefaultNamespace
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Sound
 {
-    public class Sound
-    {
-        
-    }
+    public string name;
+    public AudioClip clip;
+    [Range(0f,1f)]
+    public float volume;
+    public bool loop;
+    [HideInInspector]
+    public AudioSource source;
 }
